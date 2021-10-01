e84b9178a344c827870205093a7f1cc0cee3fae3
sreelakshmig009 <sreelakshmig009@gmail.com>
2021-10-01 21:01:38 +0530

    Plotly-Reactjs graph

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ApiChart from './ApiChart';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <ApiChart/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
